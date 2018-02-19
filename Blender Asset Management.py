30b2b34245bdaf1069cf66ff3179e5ece42543a8
AdamEarle <MrAdamEarle@gmail.com>
2018-02-19 19:16:05 +1100

    Setup
    Setting up a basic  overview

---
+++ Blender Asset Management.py
@@ -0,0 +1,49 @@
+'''
+OVERVIEW:
+
+To create a social 3D working enviroemnt that helps so mulitple  artist can work one place
+collaberatively
+
+The 3D experience of Google Docs & Drawpile
+
+
+TO DO:
+    1. Take a look at drawpile code
+    2. Assign Managers
+    3.Send link Assign
+    4. Sign in & sign out
+    5. Omni Scene, Scenes & Seats: Your either a particpant in the scnene  or your watching
+        Scene Taging :
+            TD
+            Producer
+            Simulations
+            Art Director
+            Layout
+            Materials/ surfacing
+            Lighting
+            Rendering
+            Modeling
+            Editorial
+            Animation
+            Boarding
+            story
+            Illustration
+            Concept
+            Mindmap
+            Writing
+    6. Private (Scene) public(Scene) & spectator sessions (Seat)
+    7. Assign Scene to Artist (Artist Studio Space ASS)
+    8. Invite to space
+    9. Save Artist Studios Space
+    10. Fork Scnene
+    11. Merging Scene
+
+    12. Pickup, Putting down & Place
+    13. Artist tags
+    14. Annotations: Written, Audio & Video (look at OBS Sourcecode for some ideas)
+
+    15. Instant messaging (if not the same room)
+    16. Editorial Hookup: sending finals
+
+
+'''
